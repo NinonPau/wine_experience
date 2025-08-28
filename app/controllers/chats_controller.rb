fa53171cc1006076f6260689ee1f4b79e0b1aced
Carlo Ackermann <carlo-ackermann@gmx.de>
2025-08-28 16:20:51 +0200

    Add Tokens to Messages-DB and update Chat-Controller

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -9,11 +9,17 @@ class ChatsController < ApplicationController
 
   def show
     @chat = Chat.find(params[:id])
+    # @chat = Chat.includes(:messages).find(params[:id])
+    if Rails.env.development?
+      @input_tokens = @chat.messages.pluck(:input_tokens).compact.sum
+      @output_tokens = @chat.messages.pluck(:output_tokens).compact.sum
+      @context_window = RubyLLM.models.find(@chat.model_id).context_window
+    end
     @message = Message.new
   end
 
   def create
-    @chat = Chat.new(chat_title: Date.today.to_s)
+    @chat = Chat.new(chat_title: Date.today.to_s , model_id: "gpt-4.1-nano")
     if @chat.save
       redirect_to chat_path(@chat)
     else
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_08_28_135425) do
+ActiveRecord::Schema[7.1].define(version: 2025_08_28_140746) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,6 +27,8 @@ ActiveRecord::Schema[7.1].define(version: 2025_08_28_135425) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "chat_id", null: false
+    t.integer "input_tokens"
+    t.integer "output_tokens"
     t.index ["chat_id"], name: "index_messages_on_chat_id"
   end
 
