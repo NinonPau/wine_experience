e40551e41044cc131a70df225b89aeea1bba0703
Carlo Ackermann <carlo-ackermann@gmx.de>
2025-08-28 17:12:08 +0200

    Add Chatbot

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,17 @@
 class MessagesController < ApplicationController
   before_action :set_chat, only: %i[new create]
 
+  SYSTEM_PROMPT = <<~PROMPT
+    You are a Sommelier.
+
+    I am a person looking for wine recommendations or general information.
+
+    Give me the name, region, vintage, grape type, and taste profile.
+
+    Answer consicely with bullet points in markdown.
+
+  PROMPT
+
   def new
     # @chat = Chat.find(params[:chat_id])
     @message = Message.new
@@ -8,8 +19,12 @@ class MessagesController < ApplicationController
 
   def create
     @message = Message.new(message_params)
+    @chat = Chat.find(params[:chat_id])
     @message.chat = @chat
+    @message.role = "user"
     if @message.save
+      @response = RubyLLM.chat.with_instructions(SYSTEM_PROMPT).ask(@message.content)
+      Message.create(role: "Maître Sommelier", content: @response.content, chat: @chat)
       redirect_to chat_path(@chat)
     else
       render :new, status: :unprocessable_entity
@@ -17,6 +32,10 @@ class MessagesController < ApplicationController
   end
 
   private
+
+  # def instructions
+  #   [SYSTEM_PROMPT, @chat.challenge.system_prompt].compact.join("\n\n")
+  # end
 
   def set_chat
     @chat = Chat.find(params[:chat_id])
